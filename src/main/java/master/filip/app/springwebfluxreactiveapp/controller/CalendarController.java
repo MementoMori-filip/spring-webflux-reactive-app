ad6d97f247425ab597696a294130cbf676dc8b56
LordSaruman <filip.ivanovic.wp@gmail.com>
2018-09-23 16:56:51 +0200

    3.1.1

--- src/main/java/master/filip/app/springwebfluxreactiveapp/controller/CalendarController.java
+++ src/main/java/master/filip/app/springwebfluxreactiveapp/controller/CalendarController.java
@@ -1,7 +1,9 @@
 package master.filip.app.springwebfluxreactiveapp.controller;
 
+import master.filip.app.springwebfluxreactiveapp.ambiguous.EventCustom;
 import master.filip.app.springwebfluxreactiveapp.domain.*;
 import master.filip.app.springwebfluxreactiveapp.repository.event.EventRepository;
+import master.filip.app.springwebfluxreactiveapp.repository.eventCustom.EventCustomFullReporsitory;
 import master.filip.app.springwebfluxreactiveapp.security.CurrentUser;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -9,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -19,55 +20,18 @@ import java.util.*;
 public class CalendarController {
 
     private final EventRepository eventRepository;
-    private final PasswordEncoder passwordEncoder;
-    private static final UUID ADMIN_IDENTIFIER = UUID.fromString("0d2c04f1-e25f-41b5-b4cd-3566a081200f");
+    private final EventCustomFullReporsitory eventCustomFullReporsitory;
 
-    public CalendarController(EventRepository eventRepository, PasswordEncoder passwordEncoder) {
+    public CalendarController(EventRepository eventRepository, EventCustomFullReporsitory eventCustomFullReporsitory) {
         super();
         this.eventRepository = eventRepository;
-        this.passwordEncoder = passwordEncoder;
+        this.eventCustomFullReporsitory = eventCustomFullReporsitory;
     }
 
     @GetMapping("/calendar")
     public String getCalendar(final Model model, @CurrentUser User user){
 
-        /*final Flux<Event> allEvents = this.eventRepository.findAll();*/
-        List<Event> allEvents = new ArrayList<>();
-
-        User u1 = new User(ADMIN_IDENTIFIER, "filip", passwordEncoder.encode("filip94"), "admin");
-
-        Group g2 = new Group(
-                "Java Team"
-        );
-
-        Company c2 = new Company(
-                "msg",
-                "Omladinskih brigada 90g",
-                Arrays.asList(
-                        new Group("Java Team"),
-                        new Group(".NET Team")
-                )
-        );
-
-        Member m1 = new Member(
-                "Filip Ivanovic",
-                "filipivanovic@outlook.com",
-                u1,
-                g2,
-                c2
-        );
-
-        Event e1 = new Event(
-                "proba1",
-                "probaproba",
-                new Date(),
-                new Date(),
-                "location",
-                new StyleOfEvent("public"),
-                new TypeOfEvent("birthday"),
-                Arrays.asList(m1)
-        );
-        allEvents.add(e1);
+        final Flux<EventCustom> allEvents = this.eventCustomFullReporsitory.findAll();
 
         model.addAttribute("allEvents", allEvents);
 
